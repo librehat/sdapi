60610c76bd2926b56ee3292531788235c4e0d4a4
Symeon Huang <hzwhuang@gmail.com>
2020-04-11 12:53:19 +0100

    Use rollup to create the bundle that can be used in the browser

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/sdapi.js',
+    format: 'es'
+  },
+  plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: false
+    }),
+    commonjs(),
+    typescript({
+      tsconfig: false
+    })
+  ]
+};
